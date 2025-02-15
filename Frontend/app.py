6d8181b76ddae2f392f76f1f1d85052ba3571c9a
ChickenQuail <ilikeleeches@gmail.com>
2025-02-15 11:23:04 -0800

    added html

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
-app = Flask(__name__)
+import os
+app = Flask(__name__, template_folder=os.path.join('Frontend', 'templates'))
+
 
 
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api')
 def chicken():
-    return jsonify ({"message": "quack"})
+    return jsonify({"message": "quack"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
