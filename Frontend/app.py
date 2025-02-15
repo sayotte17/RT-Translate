e1895cef37b7729bd6e913bb94fa6832bdb0d2af
ChickenQuail <ilikeleeches@gmail.com>
2025-02-15 11:06:20 -0800

    Update app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def chicken():
-    return 'quack'
+    return jsonify ({"message": "quack"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
