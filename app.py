d007a109d272d2960bb56f7e1d2a73e622ce4ab0
ChickenQuail <ilikeleeches@gmail.com>
2025-02-15 11:29:38 -0800

    made html work

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import Flask, jsonify, render_template
 import os
 app = Flask(__name__, template_folder=os.path.join('Frontend', 'templates'))
 
-
-
 @app.route('/')
 def home():
     return render_template('index.html')
