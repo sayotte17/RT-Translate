2c702d986cbdeb7f53284703fe8d6c3f91e8bfde
sayotte17 <156842420+sayotte17@users.noreply.github.com>
2025-02-15 15:57:34 -0800

    AJDNAJFHJUDFNSD

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,11 @@
 import deepl, whisper, os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import ssl
+import urllib.request
+
+# Disable SSL verification globally
+ssl._create_default_https_context = ssl._create_unverified_context
 
 app = Flask(__name__)
 CORS(app)
