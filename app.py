12705d905ad287e046f1f01bde4cd170d7675da6
ChickenQuail <ilikeleeches@gmail.com>
2025-02-15 16:12:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder=os.path.join('Frontend', 'templates'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('translator.html')
 
 @app.route('/api')
 def chicken():
